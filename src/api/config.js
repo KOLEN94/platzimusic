51829e1126c25d7199cb3632b8249779e745e6dd
KOLLEN94 <breaknius@gmail.com>
2018-05-24 14:49:06 -0500

    primer commit

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+// Application name	platzimusic
+// API key	d515f3245046c0bdde30c33ad71bb889
+// Shared secret	3d6b1e1db41d993fbf61686264d117bf
+// Registered to	KOLLEN94
+export default{
+  appName:'platzimusic',
+  apiKey: 'd515f3245046c0bdde30c33ad71bb889',
+  secret: '3d6b1e1db41d993fbf61686264d117bf',
+  registeredTo: 'KOLLEN94'
+}
